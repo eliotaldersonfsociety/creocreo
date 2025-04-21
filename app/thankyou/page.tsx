fe551d9be2e52e57222f7b42244ebe4e75492947
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-04-21 12:41:13 -0500

    Update page.tsx

--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -1,6 +1,12 @@
 // app/thankyou/page.tsx
+import React, { Suspense } from 'react';
 import OrderConfirmation from "@/components/OrderConfirmation";
 
+// Componente que maneja el suspense y la carga del componente cliente
 export default function Page() {
-  return <OrderConfirmation />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <OrderConfirmation />
+    </Suspense>
+  );
 }
